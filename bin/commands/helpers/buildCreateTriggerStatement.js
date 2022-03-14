1615ae773c15de37465a381eea240e5163f3d5fd
Nico Gallinal <nico@rematter.com>
2022-03-14 09:10:09 -0300

    escape table and column names when creating trigger, try keeping name trigger under 63 chars

--- bin/commands/helpers/buildCreateTriggerStatement.js
+++ bin/commands/helpers/buildCreateTriggerStatement.js
@@ -5,12 +5,12 @@ const buildTriggerName_1 = require("./buildTriggerName");
 const buildCreateTriggerStatement = (primaryTable, primaryKey, foreignTable, foreignKey) => /* sql */ `
   CREATE TRIGGER ${(0, buildTriggerName_1.buildTriggerName)(primaryTable, foreignTable)}
   AFTER UPDATE
-  ON ${primaryTable} FOR EACH ROW
+  ON \`${primaryTable}\` FOR EACH ROW
   BEGIN
-    IF OLD.deletedAt IS NULL AND NEW.deletedAt IS NOT NULL THEN
-      UPDATE ${foreignTable}
-        SET ${foreignTable}.deletedAt = NOW()
-      WHERE ${foreignTable}.${foreignKey} = NEW.${primaryKey};
+    IF \`OLD\`.\`deletedAt\` IS NULL AND \`NEW\`.\`deletedAt\` IS NOT NULL THEN
+      UPDATE \`${foreignTable}\`
+        SET \`${foreignTable}\`.\`deletedAt\` = NOW()
+      WHERE \`${foreignTable}\`.\`${foreignKey}\` = \`NEW\`.\`${primaryKey}\`;
     END IF;
   END;
 `;
--- bin/commands/helpers/buildTriggerName.js
+++ bin/commands/helpers/buildTriggerName.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildTriggerName = void 0;
-const buildTriggerName = (primaryTable, foreignTable) => `on_${primaryTable}_delete_update_${foreignTable}`;
+// MYSQL has a limit of 63 chars on triggers name
+// if table 'big' we remove the vowels hoping the trigger name will fit in 63 chars
+const MAX_TABLE_NAME_LENGTH = 24;
+const buildTriggerName = (primaryTable, foreignTable) => `on_${primaryTable.length > MAX_TABLE_NAME_LENGTH
+    ? primaryTable.replace(/[aeiou]/g, '')
+    : primaryTable}_del_upd_${foreignTable.length > MAX_TABLE_NAME_LENGTH
+    ? foreignTable.replace(/[aeiou]/g, '')
+    : foreignTable}`;
 exports.buildTriggerName = buildTriggerName;
